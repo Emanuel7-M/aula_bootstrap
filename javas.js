833a440c59023bc22d89031c346dcb9e2f1aa0b7
Emanuel7-M <ricksilvasilva460@gmail.com>
2021-07-01 17:29:08 -0300

    Primeiro commit

--- javas.js
+++ javas.js
@@ -1,50 +1,51 @@
-window.alert("aa")
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+
 let snake = [];
 snake[0] = {
     x:8 * box,
     y:8 * box
 }
+
 let direction = "right";
 let food = {
-    x:Math.floor(Math.ramdom() * 15 + 1) * box,
+    x: Math.floor(Math.ramdom() * 15 + 1) * box,
     y: Math.floor(Math.ramdom() * 15 + 1) * box,
 }
+// Passo1: Fundo
 function criarBG() {
-    context.fillStyle = "lightgreen"
-    context.fillRect = (0,0,16 * box, 16* box);
+    ctx.fillStyle = "lightpink"; // cor de fundo
+    ctx.fillRect = (0,0,16 * box, 16* box);
 }
+// Passo2: Criar a cobrinha
 function criarCobrinha() {
-   for(i=0; i < snake.length; i++){
-       context.fillStyle = "green";
+   for(i = 0 ; i < snake.length ; i++ ){
+       context.fillStyle = "pink";
        context.fillRect(snake[i].x, snake[i].y, box, box)
    }
 }
+// Criar a comida
 function drawFood() {
-    context.fillStyle = "red"
-    context.fillRect(food.x, food.y, box, box)
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box); 
 }
 document.addEventListener("keydown", update);
+// Faz a cobrinha não sumir
 function update (event) {
     if(event.keyCode == 37 && direction != "right") direction = "left";
     if(event.keyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
-    if(event.keyCode == 40 && direction != "up") direction = "down"
+    if(event.keyCode == 40 && direction != "up") direction = "down";
 }
-alert ("ess")
+// Inicia o jogo
     function iniciarJogo() {
-        
-        criarBG();
-        criarCobrinha();
-
         let snakeX = snake[0].x;
         let snakeY = snake[0].y;
-         for(i = 1: 1 < snake.length; i++){
+         for(i = 1; 1 < snake.length; i++) {
              if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
                  clearInterval(jogo);
-                 alert ('Gamer Over')
+                 alert ('Gamer Over');
              }
          }   
 
@@ -55,15 +56,15 @@ alert ("ess")
        
         drawFood()
     if(direction == "right") snakeX += box;
-    if(direction == "left") snakeX -= box
+    if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
-    if(snakeX != food.x || snakeY != food.y)
+    if(snakeX != food.x || snakeY != food.y);
     {
-        snake.pop()
+        
     }
-    else{food.x:Math.floor(Math.ramdom() * 15 + 1) * box,
-        food.y: Math.floor(Math.ramdom() * 15 + 1) * box
+     else {food.x;Math.floor(Math.ramdom() * 15 + 1) * box,;
+        food.y ; Math.floor(Math.ramdom() * 15 + 1) * box;
 
     }
     snake.pop(); 
